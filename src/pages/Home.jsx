50ed25a8b8b1cd25554e6d8d055212023a4e3abb
Min-Seok-Kim <70834609+Min-Seok-Kim@users.noreply.github.com>
2025-08-08 17:31:40 +0900

    [메인 페이지]
    캘린더 추가

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,14 @@
 import Layout from "./Layout";
 import { useEffect, useState } from "react";
 import axios from "../utils/axiosInstance";
+import Calendar from "react-calendar";
+import "react-calendar/dist/Calendar.css";
+import "../css/CustomCalendar.css";
 
 export default function Home() {
   const [monthLog, setMonthLog] = useState(0);
   const [yearLog, setYearLog] = useState(0);
+  const [value, setValue] = useState(new Date());
 
   useEffect(() => {
     axios
@@ -19,6 +23,8 @@ export default function Home() {
       .then((res) => setYearLog(res.data))
       .catch((err) => console.error("불러오기 실패", err));
   });
+
+  const workoutDates = ["2025-08-05", "2025-08-22"];
 
   return (
     <Layout>
@@ -36,6 +42,18 @@ export default function Home() {
           </h2>
           <p className="text-3xl font-bold text-blue-500">{yearLog}회</p>
         </div>
+
+        <Calendar
+          onChange={setValue}
+          value={value}
+          tileClassName={({ date }) => {
+            const dateStr = date.toISOString().split("T")[0];
+            if (workoutDates.includes(dateStr)) {
+              return "workout-day";
+            }
+            return null;
+          }}
+        />
       </div>
     </Layout>
   );
