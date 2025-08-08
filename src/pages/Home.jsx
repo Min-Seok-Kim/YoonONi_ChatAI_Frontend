15b29575b97dfecffb7bd5f002c2bb8906fd756a
Min-Seok-Kim <70834609+Min-Seok-Kim@users.noreply.github.com>
2025-08-08 17:06:54 +0900

    메인 페이지
    - 올해 운동 횟수

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,22 +3,39 @@ import { useEffect, useState } from "react";
 import axios from "../utils/axiosInstance";
 
 export default function Home() {
-  const [log, setLog] = useState(0);
+  const [monthLog, setMonthLog] = useState(0);
+  const [yearLog, setYearLog] = useState(0);
 
   useEffect(() => {
     axios
       .get("/log/count")
-      .then((res) => setLog(res.data))
+      .then((res) => setMonthLog(res.data))
       .catch((err) => console.error("불러오기 실패", err));
   }, []);
 
+  useEffect(() => {
+    axios
+      .get("/log/count/year")
+      .then((res) => setYearLog(res.data))
+      .catch((err) => console.error("불러오기 실패", err));
+  });
+
   return (
     <Layout>
-      <div className="bg-white shadow-md rounded-2xl p-6 w-64">
-        <h2 className="text-xl font-bold text-gray-800 mb-2">
-          이번 달 운동 횟수
-        </h2>
-        <p className="text-3xl font-bold text-blue-500">{log}회</p>
+      <div className="flex gap-4">
+        <div className="bg-white shadow-md rounded-2xl p-6 w-64">
+          <h2 className="text-xl font-bold text-gray-800 mb-2">
+            이번 달 운동 횟수
+          </h2>
+          <p className="text-3xl font-bold text-blue-500">{monthLog}회</p>
+        </div>
+
+        <div className="bg-white shadow-md rounded-2xl p-6 w-64">
+          <h2 className="text-xl font-bold text-gray-800 mb-2">
+            이번 년도 운동 횟수
+          </h2>
+          <p className="text-3xl font-bold text-blue-500">{yearLog}회</p>
+        </div>
       </div>
     </Layout>
   );
