7bce93df8fc118bc854fcec060dcfff297ef39ac
Min-Seok-Kim <70834609+Min-Seok-Kim@users.noreply.github.com>
2025-09-12 17:15:35 +0900

    수정
    - 배포 주소 추가

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:8084",
+  baseURL: "http://52.78.232.199:8084",
   headers: {
     "Content-Type": "application/json",
   },
